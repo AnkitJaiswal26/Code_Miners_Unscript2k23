cc05bd32bf1e5b39f22e997ada3c875993be343a
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2023-02-11 12:30:24 +0530

    Added contract

---
+++ contracts/CompanyNFT.sol
@@ -0,0 +1,330 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract CompanyNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    mapping(uint256 => string[]) itemsHistory;
+    enum State {
+        Manufactured, // 0
+        SoldToSeller,
+        Packed,
+        SellerShipped,
+        SellerRecieved,
+        Sold, // 4
+        Shipped, // 5
+        Received // 6
+    }
+
+    event Harvested(uint256 upc);
+    event Processed(uint256 upc);
+    event Packed(uint256 upc);
+    event ForSale(uint256 upc);
+    event Sold(uint256 upc);
+    event Shipped(uint256 upc);
+    event Received(uint256 upc);
+    event Purchased(uint256 upc);
+
+    struct Product {
+        uint256 productId;
+        string name;
+        uint256 price;
+        address company;
+    }
+
+    struct ProductItem {
+        uint256 upc;
+        address payable ownerID;
+        address payable companyAdd;
+        uint256 productID;
+        string quality;
+        uint256 productPrice;
+        State itemState;
+        address payable distributorAdd;
+        address payable retailerAdd;
+        address payable consumerAdd;
+        string cid;
+        string privateKey;
+        string pubKey;
+    }
+
+    address private company;
+    string companyName;
+    string cin;
+
+    address[] private products;
+    uint256 private _totalSupply;
+    uint256 private productCount;
+
+    mapping(uint256 => ProductItem) items;
+    mapping(uint256 => Product) private productsMapping;
+    mapping(string => uint256) privateKeyToProductItemMapping;
+    mapping(string => string) pubKeyToPrivateKeyMapping;
+
+    constructor(
+        string memory _companyName,
+        string memory _cin,
+        string memory companySymbol,
+        address owner
+    ) ERC721(_companyName, companySymbol) {
+        company = owner;
+        companyName = _companyName;
+        cin = _cin;
+    }
+
+    function addProduct(string memory name, uint256 price) public {
+        productsMapping[productCount++] = Product(
+            productCount,
+            name,
+            price,
+            msg.sender
+        );
+    }
+
+    function fetchCompanyDetails()
+        public
+        view
+        returns (address, string memory, string memory)
+    {
+        return (company, companyName, cin);
+    }
+
+    function mint(
+        uint256 productID,
+        string memory quality,
+        uint256 price,
+        State state,
+        string memory pubKey,
+        string memory privateKey,
+        string memory cid
+    ) public {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, cid);
+
+        items[newItemId] = ProductItem({
+            upc: newItemId,
+            ownerID: payable(msg.sender),
+            companyAdd: payable(msg.sender),
+            productID: productID,
+            quality: quality,
+            productPrice: price,
+            itemState: state,
+            distributorAdd: payable(address(0)),
+            retailerAdd: payable(address(0)),
+            consumerAdd: payable(address(0)),
+            cid: cid,
+            privateKey: privateKey,
+            pubKey: pubKey
+        });
+
+        privateKeyToProductItemMapping[privateKey] = newItemId;
+        pubKeyToPrivateKeyMapping[pubKey] = privateKey;
+    }
+
+    function addBulkProducts(
+        uint256 productId,
+        string[] memory quality,
+        uint256 price,
+        string[] memory pubKeys,
+        string[] memory privateKeys,
+        string[] memory cids
+    ) public {
+        for (uint256 i = 0; i < pubKeys.length; i++) {
+            mint(
+                productId,
+                quality[i],
+                price,
+                State.Manufactured,
+                pubKeys[i],
+                privateKeys[i],
+                cids[i]
+            );
+        }
+    }
+
+    function growNFT(
+        uint256 tokenId,
+        string memory tokenURI,
+        State state
+    ) private {
+        require(
+            items[tokenId].itemState == State.Received,
+            "It id impossible to grow more"
+        );
+
+        _setTokenURI(tokenId, tokenURI);
+
+        if (state == State.SellerRecieved) {
+            items[tokenId].retailerAdd = payable(msg.sender);
+        } else if (state == State.SellerShipped) {
+            items[tokenId].distributorAdd = payable(msg.sender);
+        }
+
+        if (state == State.SoldToSeller && state == State.Sold) {
+            items[tokenId].ownerID = payable(msg.sender);
+        }
+
+        items[tokenId].cid = tokenURI;
+        items[tokenId].itemState = state;
+    }
+
+    function scanAndGrow(
+        string memory pubKey,
+        string memory tokenURI,
+        State state
+    ) public {
+        uint256 itemId = privateKeyToProductItemMapping[
+            pubKeyToPrivateKeyMapping[pubKey]
+        ];
+        growNFT(itemId, tokenURI, state);
+    }
+
+    function buyProductForSeller(
+        uint256 itemId,
+        string memory tokenURI
+    ) public payable {
+        growNFT(itemId, tokenURI, State.SoldToSeller);
+    }
+
+    function buyBulkProductsForSeller(
+        uint256 productId,
+        uint256 count,
+        string memory tokenURI
+    ) public payable {
+        uint256 totalItemCount = _tokenIds.current();
+        for (uint256 i = 0; i < totalItemCount && count > 0; i++) {
+            if (
+                items[i].productID == productId &&
+                items[i].itemState == State.Manufactured
+            ) {
+                buyProductForSeller(i, tokenURI);
+            }
+        }
+    }
+
+    function buyProductForUser(
+        uint256 itemId,
+        string memory cid
+    ) public payable {
+        growNFT(itemId, cid, State.Sold);
+    }
+
+    function buyProduct(
+        string memory privateKey,
+        string memory tokenURI
+    ) public payable {
+        growNFT(
+            privateKeyToProductItemMapping[privateKey],
+            tokenURI,
+            State.Received
+        );
+    }
+
+    function transferNFT(
+        uint256 itemId,
+        address to,
+        string memory tokenURI
+    ) public {
+        if (items[itemId].ownerID == msg.sender) {
+            items[itemId].ownerID = payable(to);
+            items[itemId].cid = tokenURI;
+        }
+
+        _setTokenURI(itemId, tokenURI);
+    }
+
+    function fetchAllProducts() public view returns (Product[] memory) {
+        Product[] memory result = new Product[](productCount);
+
+        for (uint256 i = 0; i < productCount; i++) {
+            Product storage cur = productsMapping[i];
+            result[i] = cur;
+        }
+
+        return result;
+    }
+
+    function fetchAllProductItemsByProductIdForUser(
+        uint256 productId
+    ) public view returns (ProductItem[] memory) {
+        uint256 totalCount = _tokenIds.current();
+
+        uint256 proCount;
+        for (uint256 i = 0; i < totalCount; i++) {
+            if (
+                items[i].productID == productId &&
+                items[i].itemState == State.SellerRecieved
+            ) {
+                proCount += 1;
+            }
+        }
+
+        ProductItem[] memory result = new ProductItem[](proCount);
+        proCount = 0;
+
+        for (uint256 i = 0; i < totalCount; i++) {
+            if (
+                items[i].productID == productId &&
+                items[i].itemState == State.SellerRecieved
+            ) {
+                ProductItem storage cur = items[i];
+                result[proCount++] = cur;
+            }
+        }
+
+        return result;
+    }
+
+    function fetchAllUserProducts() public view returns (ProductItem[] memory) {
+        uint256 totalCount = _tokenIds.current();
+
+        uint256 proCount;
+        for (uint256 i = 0; i < totalCount; i++) {
+            if (items[i].ownerID == msg.sender) {
+                proCount += 1;
+            }
+        }
+
+        ProductItem[] memory result = new ProductItem[](proCount);
+        proCount = 0;
+
+        for (uint256 i = 0; i < totalCount; i++) {
+            if (items[i].ownerID == msg.sender) {
+                ProductItem storage cur = items[i];
+                result[proCount++] = cur;
+            }
+        }
+
+        return result;
+    }
+
+    function fetchProductItemById(
+        uint256 itemId
+    ) public view returns (ProductItem memory) {
+        return items[itemId];
+    }
+
+    function fetchProductItemByPrivateKey(
+        string memory privateKey
+    ) public view returns (uint256 id) {
+        return privateKeyToProductItemMapping[privateKey];
+    }
+
+    function fetchProductItemByPublicKey(
+        string memory publicKey
+    ) public view returns (uint256 id) {
+        return
+            privateKeyToProductItemMapping[
+                pubKeyToPrivateKeyMapping[publicKey]
+            ];
+    }
+}
---
+++ contracts/SupplyChain.sol
@@ -0,0 +1,271 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// import "hardhat/console.sol";
+import "./CompanyNFT.sol";
+
+contract SupplyChain {
+    address payable owner;
+
+    struct User {
+        address userAdd;
+        string name;
+        uint256 age;
+        bool gender;
+        string email;
+        string mobileNo;
+    }
+
+    struct Company {
+        address comAdd;
+        string name;
+        string cin;
+    }
+
+    struct Seller {
+        address sellerAdd;
+        string name;
+        string email;
+        string mobileNo;
+    }
+
+    receive() external payable {}
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    uint256 userCount;
+    uint256 companyRequestCount;
+    uint256 companyCount;
+    uint256 sellerCount;
+    uint256 sellerRequestCount;
+
+    mapping(uint256 => User) userMapping;
+    mapping(uint256 => Company) companyMapping;
+    mapping(uint256 => Company) companyRequestMapping;
+    mapping(uint256 => Seller) sellerMapping;
+    mapping(uint256 => Seller) sellerRequestMapping;
+    mapping(uint256 => CompanyNFT) companyNFTMapping;
+
+    mapping(address => uint256) userAddressToIdMapping;
+    mapping(address => uint256) companyAddressToIdMapping;
+    mapping(address => uint256) companyAddressToIdRequestMapping;
+    mapping(address => uint256) sellerAddressToIdMapping;
+    mapping(address => uint256) sellerAddressToIdRequestMapping;
+
+    function registerUser(
+        address userAdd,
+        string memory name,
+        string memory emailId,
+        string memory mobileNo,
+        bool gender,
+        uint256 age
+    ) public {
+        userMapping[userCount] = User(
+            userAdd,
+            name,
+            age,
+            gender,
+            emailId,
+            mobileNo
+        );
+
+        userAddressToIdMapping[msg.sender] = userCount++;
+    }
+
+    function registerCompany(
+        address comAdd,
+        string memory name,
+        string memory cin
+    ) public {
+        companyRequestMapping[companyRequestCount] = Company(comAdd, name, cin);
+        companyAddressToIdRequestMapping[msg.sender] = companyRequestCount++;
+    }
+
+    function registerSeller(
+        address sellerAdd,
+        string memory name,
+        string memory mobileNo,
+        string memory emailId
+    ) public {
+        sellerRequestMapping[sellerRequestCount] = Seller(
+            sellerAdd,
+            name,
+            emailId,
+            mobileNo
+        );
+        sellerAddressToIdRequestMapping[msg.sender] = sellerRequestCount++;
+    }
+
+    function acceptCompany(address companyAdd) public isOwner {
+        companyMapping[companyCount] = companyRequestMapping[
+            companyAddressToIdRequestMapping[companyAdd]
+        ];
+
+        companyAddressToIdMapping[companyAdd] = companyCount;
+
+        companyNFTMapping[companyCount] = new CompanyNFT(
+            companyMapping[companyCount].name,
+            companyMapping[companyCount].cin,
+            companyMapping[companyCount].cin,
+            companyMapping[companyCount].comAdd
+        );
+        companyCount += 1;
+
+        companyRequestMapping[
+            companyAddressToIdRequestMapping[companyAdd]
+        ] = companyRequestMapping[companyRequestCount - 1];
+        companyAddressToIdRequestMapping[
+            companyRequestMapping[companyRequestCount - 1].comAdd
+        ] = companyAddressToIdRequestMapping[companyAdd];
+
+        delete companyRequestMapping[companyRequestCount - 1];
+        companyRequestCount -= 1;
+    }
+
+    function rejectCompany(address companyAdd) public isOwner {
+        companyRequestMapping[
+            companyAddressToIdRequestMapping[companyAdd]
+        ] = companyRequestMapping[companyRequestCount - 1];
+        companyAddressToIdRequestMapping[
+            companyRequestMapping[companyRequestCount - 1].comAdd
+        ] = companyAddressToIdRequestMapping[companyAdd];
+
+        delete companyRequestMapping[companyRequestCount - 1];
+        companyRequestCount -= 1;
+    }
+
+    function acceptSeller(address sellerAdd) public isOwner {
+        sellerMapping[sellerCount] = sellerRequestMapping[
+            sellerAddressToIdRequestMapping[sellerAdd]
+        ];
+
+        sellerAddressToIdMapping[sellerAdd] = sellerCount;
+
+        // companyNFTMapping[companyCount] = new CompanyNFT(
+        //     companyMapping[companyCount].name,
+        //     companyMapping[companyCount].cin,
+        //     companyMapping[companyCount].cin,
+        //     companyMapping[companyCount].comAdd
+        // );
+        sellerCount += 1;
+
+        sellerRequestMapping[
+            sellerAddressToIdRequestMapping[sellerAdd]
+        ] = sellerRequestMapping[sellerRequestCount - 1];
+        sellerAddressToIdRequestMapping[
+            sellerRequestMapping[sellerRequestCount - 1].sellerAdd
+        ] = sellerAddressToIdRequestMapping[sellerAdd];
+
+        delete sellerRequestMapping[sellerRequestCount - 1];
+        sellerRequestCount -= 1;
+    }
+
+    function rejectSeller(address sellerAdd) public isOwner {
+        sellerRequestMapping[
+            sellerAddressToIdRequestMapping[sellerAdd]
+        ] = sellerRequestMapping[sellerRequestCount - 1];
+        sellerAddressToIdRequestMapping[
+            sellerRequestMapping[sellerRequestCount - 1].sellerAdd
+        ] = sellerAddressToIdRequestMapping[sellerAdd];
+
+        delete sellerRequestMapping[sellerRequestCount - 1];
+        sellerRequestCount -= 1;
+    }
+
+    function fetchUserByAddress(
+        address userAdd
+    ) public view returns (User memory) {
+        for (uint256 i = 0; i < userCount; i++) {
+            if (userMapping[i].userAdd == userAdd) {
+                return userMapping[i];
+            }
+        }
+
+        revert();
+    }
+
+    function fetchCompanyByAddress(
+        address comAdd
+    ) public view returns (Company memory) {
+        for (uint256 i = 0; i < companyCount; i++) {
+            if (companyMapping[i].comAdd == comAdd) {
+                return companyMapping[i];
+            }
+        }
+
+        revert();
+    }
+
+    function fetchActiveRequests()
+        public
+        view
+        isOwner
+        returns (Company[] memory)
+    {
+        Company[] memory result = new Company[](companyRequestCount);
+        for (uint256 i = 0; i < companyRequestCount; i++) {
+            Company storage cur = companyRequestMapping[i];
+            result[i] = cur;
+        }
+
+        return result;
+    }
+
+    function fetchAllCompanies()
+        public
+        view
+        isOwner
+        returns (Company[] memory)
+    {
+        Company[] memory result = new Company[](companyCount);
+        for (uint256 i = 0; i < companyCount; i++) {
+            Company storage cur = companyMapping[i];
+            result[i] = cur;
+        }
+
+        return result;
+    }
+
+    function fetchCompanyUsingCIN(
+        string memory cin
+    ) public view returns (Company memory) {
+        for (uint256 i = 0; i < companyCount; i++) {
+            if (
+                keccak256(abi.encodePacked(companyMapping[i].cin)) ==
+                keccak256(abi.encodePacked(cin))
+            ) {
+                return companyMapping[i];
+            }
+        }
+
+        revert();
+    }
+
+    function fetchAllCompaniesNFT() public view returns (address[] memory) {
+        address[] memory result = new address[](companyCount);
+        for (uint256 i = 0; i < companyCount; i++) {
+            address cur = address(companyNFTMapping[i]);
+            result[i] = cur;
+        }
+
+        return result;
+    }
+
+    function fetchCompanyNFTAddress(
+        address companyAddr
+    ) public view returns (address) {
+        return
+            address(companyNFTMapping[companyAddressToIdMapping[companyAddr]]);
+    }
+
+    function OwnerIs() public view returns (bool) {
+        return owner == msg.sender;
+    }
+}
